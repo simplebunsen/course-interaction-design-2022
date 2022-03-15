783cd7683b0e6f329ec06d9951fad3ed10f402ea
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-03-15 21:25:36 +0100

    basic graphic minute timer

--- Block_I/p5js/mycode/02_clock_graphic/sketch.js
+++ Block_I/p5js/mycode/02_clock_graphic/sketch.js
@@ -5,43 +5,45 @@ let h = 800;
 let seconds, milliseconds;
 
 function setup() {
-
   createCanvas(w, h);
   angleMode(DEGREES);
 }
 
 function draw() {
 
-  background(255);
+  //background(255);
 
+  //technical second calc stuff
   milliseconds = int(millis() % 60000);
   seconds = int(milliseconds / 1000);
-  //seconds = milliseconds / 1000;
 
   let s = map(seconds, 0, 60, 0, 360);
   let m = map(milliseconds, 0, 1000, 0, 360);
+  
+  let currentSize = w - ((w/60) * seconds);
+  let currentColor = map(currentSize, 0, w, 0, 255);
+  
 
-  // draw background
-  fill(0);
-  noStroke();
-  ellipse(w / 2, h / 2, w / 2, w / 2);
 
-  // milliseconds
-  push();
-  translate(w / 2, h / 2);
-  rotate(m);
-  stroke(0, 255, 0);
-  strokeWeight(5);
-  line(0, 0, 0, -80);
-  pop();
-
+  colorMode(HSB);
   // seconds
   push();
-  translate(w / 2, h / 2);
-  rotate(s);
-  stroke(255, 0, 0);
-  strokeWeight(10);
-  line(0, 0, 0, -80);
+    strokeWeight(2);
+    stroke(currentColor, 255, 90);
+    fill(currentColor,255,255);
+    ellipseMode(CORNER);
+    ellipse((w - currentSize) / 2, (h - currentSize) / 2, currentSize, currentSize);
   pop();
 
+  
+  // milliseconds
+  push();
+  translate(w/2, h/2);
+  rotate(m-90);
+  stroke(currentColor, 255, 90);
+  strokeWeight(25);
+  strokeCap(ROUND);
+  line(0, 0, currentSize/2 - 20, 0);
+
+  pop();
 }
