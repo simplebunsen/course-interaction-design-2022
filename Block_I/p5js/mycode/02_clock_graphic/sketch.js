b68d68abfdd55bdcf356658e0733ef00cd226464
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-03-16 22:42:03 +0100

    fix end of cycle bug of clock arm

--- Block_I/p5js/mycode/02_clock_graphic/sketch.js
+++ Block_I/p5js/mycode/02_clock_graphic/sketch.js
@@ -19,31 +19,36 @@ function draw() {
 
   let s = map(seconds, 0, 60, 0, 360);
   let m = map(milliseconds, 0, 1000, 0, 360);
-  
-  let currentSize = w - ((w/60) * seconds);
+
+  let currentSize = w - ((w / 60) * seconds);
   let currentColor = map(currentSize, 0, w, 0, 255);
-  
+
 
 
   colorMode(HSB);
   // seconds
   push();
-    strokeWeight(2);
-    stroke(currentColor, 255, 90);
-    fill(currentColor,255,255);
-    ellipseMode(CORNER);
-    ellipse((w - currentSize) / 2, (h - currentSize) / 2, currentSize, currentSize);
+
+  strokeWeight(2);
+  stroke(currentColor, 255, 90);
+  fill(currentColor, 255, 255);
+  ellipseMode(CORNER);
+  ellipse((w - currentSize) / 2, (h - currentSize) / 2, currentSize, currentSize);
+
   pop();
 
-  
+
   // milliseconds
   push();
-  translate(w/2, h/2);
-  rotate(m-90);
-  stroke(currentColor, 255, 90);
-  strokeWeight(25);
-  strokeCap(ROUND);
-  line(0, 0, currentSize/2 - 20, 0);
 
+  translate(w / 2, h / 2);
+  rotate(m - 90);
+  stroke(currentColor, 255, 90);
+  if (currentSize >= 0.1 * w) strokeWeight(25);
+  else strokeWeight(10);
+  strokeCap(ROUND);
+  let lineLenght = currentSize / 2 - 20 >= 0 ? currentSize / 2 - 20 : 0;
+  line(0, 0, lineLenght, 0);
+  
   pop();
 }
