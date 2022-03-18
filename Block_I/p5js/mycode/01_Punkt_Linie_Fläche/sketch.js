f753976d88f4d0fa53cb3ede2163eb485244b964
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-03-18 21:39:34 +0100

    Exercise 3 Done

--- Block_I/p5js/mycode/01_Punkt_Linie_Fläche/sketch.js
+++ Block_I/p5js/mycode/01_Punkt_Linie_Fläche/sketch.js
@@ -1,34 +1,106 @@
-let w = 600
-let h = 600;
+let w = 800
+let h = 800;
 
 let seconds, milliseconds;
+
+let currentCol;
+
+let dotArray = [];
+let oldDots = [];
+
+class Dot {
+  constructor(x, y, col) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+
+let curColor, color1, color2, color1Dark, color2Dark;
+let clrToggle, dontChange;
 
 function setup() {
 
   createCanvas(w, h);
   angleMode(DEGREES);
 
-  noStroke();
-  fill(255, 0, 0);
+  for (let y = 0; y < 6; y++) {
+    for (let x = 0; x < 10; x++) {
+      dotArray.push(new Dot(x * w / 10, y * h / 6, currentCol));
+    }
+  }
+
+  color1 = color(77, 27, 0);
+  color1Dark = color(97, 34, 0);
+
+  color2 = color(255, 231, 176);
+  color2Dark = color(247, 227, 181);
+
+  curColor = color1;
+  curColorDark = color1Dark;
+
+  clrToggle = false;
+  dontChange = true;
+
+
 }
+
+let revisions = 0;
+let regognizedRevisions = 0;
 
 function draw() {
 
-  background(255);
+  background(100);
 
   milliseconds = int(millis() % 60000);
   seconds = milliseconds / 1000;
-  //seconds = int(milliseconds / 1000);
+
+  if(seconds >= 2 && seconds <= 4) dontChange = false;
+
+  if(revisions >= 1){
+    for (let i = 0; i < dotArray.length; i++) {
+      fill(curColor == color1 ? color2 : color1);
+      rect(dotArray[i].x, dotArray[i].y, w / 10, h / 6);
+      fill(curColorDark == color1Dark ? color2Dark : color1Dark);
+      rect(dotArray[i].x + 10, dotArray[i].y + 15, w / 10 - 20, h / 6 - 30);
+    }
+  }
+
+  for (let i = 0; i < seconds; i++) {
+    fill(curColor);
+    rect(dotArray[i].x, dotArray[i].y, w / 10, h / 6);
+    fill(curColorDark);
+    rect(dotArray[i].x + 10, dotArray[i].y + 15, w / 10 - 20, h / 6 - 30);
+    // fill(255);
+    // textAlign(CENTER, CENTER);
+    // textSize(20);
+    // text(i, dotArray[i].x + w/20, dotArray[i].y + h / 12);
+  }
 
 
-  push();
-  translate(w / 2, h / 2);
-  rotate(-90);
-  fill(map(int(seconds), 0, 60, 0, 255), map(int(seconds), 0, 60, 255, 0), 0);
-  //fill(150);
-  arc(0, 0, w, w, 0, seconds * 6, PIE);
-  //fill(map(int(seconds),0,60,0,255),map(int(seconds),0,60,255,0),0);
-  //arc(0,0, w, w, int(seconds * 6) - int(seconds*6)%10, seconds*6, PIE);
-  pop();
+  //this code is so shitty I'm literally crying
+  if(seconds <= 1 && clrToggle == false && !dontChange){
+    console.log("changing to white color");
+    curColor = color2;
+    curColorDark = color2Dark;
+    clrToggle = true;
+    dontChange= true;
+  }
+  else if(seconds <= 1 && clrToggle == true && !dontChange){
+    console.log("changing to white color");
+    curColor = color1;
+    curColorDark = color1Dark;
+    clrToggle = false;
+    dontChange = true;
+  }
+
+  if(seconds >= 59 && regognizedRevisions <= revisions){
+    regognizedRevisions++;
+  }
+
+  if(seconds <= 1 && regognizedRevisions > revisions){
+    revisions++;
+  }
+
 
 }
