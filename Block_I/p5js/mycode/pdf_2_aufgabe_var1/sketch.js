d3f56d1eb0cb43d935bca10c5da8b3881ea04117
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-03-28 22:21:44 +0200

    Variant 1 und 2 done

---
+++ Block_I/p5js/mycode/pdf_2_aufgabe_var1/sketch.js
@@ -0,0 +1,50 @@
+
+let w = 800
+let h = 800;
+let seconds, milliseconds;
+
+function setup() {
+  createCanvas(w, h);
+  angleMode(DEGREES);
+  background(255);
+  rectMode(CENTER);
+}
+
+function draw() {
+
+  //technical second calc stuff
+  milliseconds = int(millis() % 60000);
+  seconds = int(milliseconds / 1000);  
+  millisecondsPerSecond = milliseconds % 1000;
+
+  let seconds_two_digits = String(seconds).padStart(2, "0");
+  let milliseconds_three_digits = String(millisecondsPerSecond).padStart(3, "0");
+
+  fill(242,242,242);
+  rect(110,35,200,50,10);
+  fill(0);
+  textSize(40);
+  textAlign(LEFT, CENTER);
+  
+  let curtime = "00:" + seconds_two_digits + "." + milliseconds_three_digits; 
+  text(curtime, 20, 40);
+
+  fill(255, 128, 255);
+  noStroke();
+  ellipse(mouseX, mouseY, 3);
+  stroke(0);
+
+}
+
+function keyPressed(){
+  fill(keyCode);
+  let x = random(50, 750);
+  let y = random(50, 750);
+  rect(x,y, 40, 40, 10);
+  if(keyCode > 150) fill(0);
+  else fill(255);
+  if(key.length > 1) textSize(10);
+  else textSize(30);
+  textAlign(CENTER, CENTER);
+  text(key, x, y);
+}
