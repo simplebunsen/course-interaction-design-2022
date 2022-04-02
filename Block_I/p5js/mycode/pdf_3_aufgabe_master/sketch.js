6a2956265b3aa4c4dcaddb7a8164bb6be3238216
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-04-02 21:26:11 +0200

    Line Color and weight random done, cleanup

--- Block_I/p5js/mycode/pdf_3_aufgabe_master/sketch.js
+++ Block_I/p5js/mycode/pdf_3_aufgabe_master/sketch.js
@@ -1,23 +1,55 @@
-let offsetsLeft = [];
+"use strict";
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+//changeable properties
+  const lineAmountPer = 30;
 
+
+
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+//all lines aray
 let lines = [];
 
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+//define helper functions
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 function setup() {
   createCanvas(800, 800);
-  for (let i = 0; i < 20; i++) {
-    lines[i] = new line(random(120,280));
+
+  let blueColors = [color("#30697E"), color("#258DA0"), color("#11B2BD"), color("#1ED8D2"), color("#4CFFDF"), color("#97B0AA")];
+  let redColors = [color("#F64747"), color("#FF6C82"), color("#FF94BA"), color("#FFBDEE"), color("#BFA6A2"), color("#4E0000")];
+
+  //red
+  for (let i = 0; i < lineAmountPer; i++) {
+    lines.push(new line(random(120,280), redColors[getRandomInt(0, redColors.length)]));
+  }
+
+  //blue
+  for (let i = 0; i < lineAmountPer; i++) {
+    lines.push(new line(random(520,680), blueColors[getRandomInt(0, blueColors.length)]));
   }
 }
 
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 function draw() {
-  background(200, 200, 200);
+  background(255);
 
   noFill();
 
   for (let lineNo = 0; lineNo < lines.length; lineNo++) {
+    stroke(lines[lineNo].colour);    
+    strokeWeight(lines[lineNo].weight);
     beginShape();
+    
 
     for (let i = 0; i < lines[lineNo].getCtrlPoints.length; i++) {
       curveVertex(lines[lineNo].getCtrlPoints[i].getCurPos, (i - 1) * 100);
@@ -27,15 +59,13 @@ function draw() {
   }
 }
 
-function getRndChange() {
-  return random(-2.0, 2.0);
-}
-
-
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 //Line mit verschiedenen Punkten
 class line {
-  constructor(pos){
+  constructor(pos, colour){
     this.ctrlPoints = [];
+    this.colour = colour;
+    this.weight = random(0.1,1.5);
     for(let i = 0; i < 11; i++){
       this.ctrlPoints[i] = new ctrlPoint(pos);
     }
@@ -43,11 +73,10 @@ class line {
 
   get getCtrlPoints(){
     return this.ctrlPoints;
-  }
-  
+  }  
 }
 
-
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 // Einzelner Punkt in einer Line
 class ctrlPoint {
   constructor(center) {
@@ -88,3 +117,4 @@ class ctrlPoint {
     return this.curPos;
   }
 }
+//<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
