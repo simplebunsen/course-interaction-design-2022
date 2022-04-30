ccdf7f80cfb9952a54109e89f7102a98aa1b8931
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-04-30 12:52:36 +0200

    first pass on rythm game

---
+++ Block_II/p5js/examples/mycode/pdf6_try_a/sketch.js
@@ -0,0 +1,204 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+let threshold = 30;
+let accChangeX = 0;
+let accChangeY = 0;
+let accChangeT = 0;
+
+let fS = 24;
+
+let pShakeMs = 0;
+let shakeSafetyDelay = 200;
+
+let interShakeDurations = [];
+
+let shakesProcessed = 0;
+
+let currentProcessedBeatDelay = 0;
+
+
+
+let osc;
+let playing;
+let freq = 100;
+let amp = .1;
+
+const State = {
+  Splash: 'Splash',
+  Countdown: 'Countdown',
+  Record: 'Record',
+  Play: 'Play'
+};
+var currentState = State.Record;
+
+function setup() {
+  canvas = createCanvas(w, h);
+  textSize(fS * 0.8);
+  checkSensorPermissions();
+
+  osc = new p5.Oscillator('triangle');
+  osc.freq(freq);
+  osc.amp(amp);
+
+}
+
+function draw() {
+  background(255);
+
+  switch (currentState) {
+    case State.Splash:
+
+      break;
+    case State.Countdown:
+
+      break;
+    case State.Record:
+      text("recording, shake or click in beat", 20, height/2);
+      if (checkForShake()) {
+        processShake();
+      }
+
+      let they = 40;
+      interShakeDurations.forEach(e => {
+        text('element: ' + e, 20, they);
+        they += 10;
+      });
+
+      if (shakesProcessed >= 5) {
+        console.log("we full na")
+        let sum = 0;
+        interShakeDurations.forEach(e => sum += e);
+        sum = sum / interShakeDurations.length;
+        currentProcessedBeatDelay = sum;
+        currentState = State.Play;
+
+        fill("red");
+        setInterval(() => {
+          console.log("printing rect");
+          rect(width / 2, height / 2, 50, 50);
+          playOscillator();
+          setTimeout(rampDownOscillator, 50);
+        }, currentProcessedBeatDelay);
+      }
+      break;
+    case State.Play:
+      text("playing back ur beat boi", 20, height/2);
+      break;
+    default:
+      break;
+  }
+
+  if (playing) {
+    // smooth the transitions by 0.1 seconds
+    osc.freq(freq, 0.1);
+    osc.amp(amp, 0.1);
+  }
+
+}
+
+function checkForShake() {
+  // Calculate total change in accelerationX and accelerationY
+  accChangeX = abs(accelerationX - pAccelerationX);
+  accChangeY = abs(accelerationY - pAccelerationY);
+  accChangeZ = abs(accelerationZ - pAccelerationZ);
+  accChangeT = accChangeX + accChangeY + accChangeZ;
+
+  // If shake
+  if (accChangeT >= threshold) return true;
+  // If not shake
+  else return false;
+}
+
+function processShake() {
+  console.log("simulating shake");
+  if (pShakeMs == 0) {
+    console.log("inside pshakems == 0")
+    pShakeMs = millis();
+    playOscillator();
+    setTimeout(rampDownOscillator, 50);
+  } else if (millis() > pShakeMs + shakeSafetyDelay) {
+    console.log("getting inside array push " + (millis() - pShakeMs));
+    interShakeDurations.push(millis() - pShakeMs);
+    pShakeMs = millis();
+    playOscillator();
+    setTimeout(rampDownOscillator, 50);
+  } else console.log("inside neither");
+
+  shakesProcessed += 1;
+}
+
+function mousePressed() {
+  processShake();
+}
+
+function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
+  userStartAudio();
+  osc.start();
+  playing = true;
+}
+
+function rampDownOscillator() {
+  // ramp amplitude to 0 over 0.1 seconds
+  console.log("ramping down");
+  osc.amp(0, 0.3);
+  playing = false;
+}
+
+// ----------------------------------------------
+// https://editor.p5js.org/PaulGSA/sketches/Ezs-7rM3Y
+// ----------------------------------------------
+// SENSOR PERMISSION MANAGER FOR iOS13 AND SAFARI
+// THANKS TO Dae In Chung for his video here:
+// https://youtu.be/AbB9ayaffTc
+let sensorPermissionGranted = false;
+
+function checkSensorPermissions() {
+  // DeviceOrientationEvent and DeviceMotionEvent
+  if (typeof (DeviceOrientationEvent) !== 'undefined' &&
+    typeof (DeviceOrientationEvent.requestPermission) === 'function') {
+    // Must be iOS device so we need to ask permission to use sensors
+    DeviceOrientationEvent.requestPermission()
+      .catch(() => {
+        // show permission dialogue only first time
+        // This button label can be customised
+        let button = createButton("Allow access to sensors");
+        button.style("font-size", "48px");
+        button.center();
+        button.mousePressed(requestAccess);
+        throw error;
+      })
+      .then(() => {
+        // already granted permission
+        sensorPermissionGranted = true;
+      })
+  } else {
+    // Non-iOS13 device so no worries
+    sensorPermissionGranted = true;
+  }
+}
+
+function requestAccess() {
+  DeviceOrientationEvent.requestPermission()
+    .then(response => {
+      if (response == 'granted') {
+        // User has granted permission to access sensors
+        sensorPermissionGranted = true;
+      } else {
+        // User has denied permission to access sensors
+        sensorPermissionGranted = false;
+      }
+    })
+    .catch(console.error);
+  this.remove(); // kills button after user interaction
+}
+
+function windowResized() {
+  // assigns new values for width and height variables
+  w = window.innerWidth;
+  h = window.innerHeight;
+  resizeCanvas(w, h);
+}
