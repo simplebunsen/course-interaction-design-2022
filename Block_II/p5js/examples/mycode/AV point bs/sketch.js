50be8509831a92bbc44176445735f723907fc6dc
rappluk <52760596+simplebunsen@users.noreply.github.com>
2022-05-07 20:31:48 +0200

    impfugn

--- Block_II/p5js/examples/mycode/AV point bs/sketch.js
+++ Block_II/p5js/examples/mycode/AV point bs/sketch.js
@@ -27,17 +27,22 @@ function keyPressed() {
   if (key === "b") {
     console.log("toggled bg");
     isBlack = !isBlack;
-  } else if (keyCode === UP_ARROW){
+  } else if (keyCode === UP_ARROW) {
     radius = (radius + 75) % 1000;
     console.log(radius);
-  }
-  else if (keyCode === DOWN_ARROW){
+  } else if (keyCode === DOWN_ARROW) {
     radius = (radius - 75) % 1000;
     console.log(radius);
+  } else if (keyCode === LEFT_ARROW) {
+    resizeCanvas(1600, 900);
+    console.log("resized to widescreen");
+  } else if (keyCode === RIGHT_ARROW) {
+    resizeCanvas(800, 800);
+    console.log("resized to square");
   }
 
 }
 
-function mouseClicked(){
+function mouseClicked() {
   saveCanvas(canvas, "screenie", "jpg")
 }
