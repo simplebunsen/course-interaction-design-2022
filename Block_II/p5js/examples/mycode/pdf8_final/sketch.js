3d6da0568c0e832d7cb9da09fc2d7070da73e9fc
Lukas Rapp <52760596+simplebunsen@users.noreply.github.com>
2022-05-16 00:21:52 +0200

    finaln

--- Block_II/p5js/examples/mycode/pdf8_final/sketch.js
+++ Block_II/p5js/examples/mycode/pdf8_final/sketch.js
@@ -2,7 +2,9 @@ let canvas;
 
 let showWarning = false;
 
-setTimeout(function() {showWarning = true;}, 60000);
+setTimeout(function () { showWarning = true; }, 60000);
+
+setTimeout(function () { location.reload(); }, 63000);
 
 
 let sketch = function (p) {
@@ -31,6 +33,8 @@ let sketch = function (p) {
 
 
   var depthImg;
+
+  var tromboneRate = 1;
 
 
 
@@ -67,30 +71,40 @@ let sketch = function (p) {
 
 
 
+  let tromboneHUDworks = false;
+  let drumsHUDworks = false;
 
-
-
+  let loadingStill = true;
 
   p.draw = function () {
     p.clear();
     p.background(255);
-    p.textSize(40);
+    p.textSize(scaling*30);
     p.noStroke();
-    if(showWarning) p.text("p5 sound becomes laggy after time, \n please reload site in case of bugs", 20, 50);
-
-
-
+    p.fill(0);
+    p.textFont("Helvetica")
+    p.textAlign(p.CENTER, p.CENTER);
+    if (showWarning) p.text("reloading in 3 seconds", p.width/2, 50);
+    if (loadingStill) {
+      p.text("...loading...", p.width / 2, 100);
+    }
     p.drawDepthIndicator(firstRecogMs);
     p.drawRecognNotice(firstRecogMs);
-    
+
     let timeNow = p.millis();
 
     if (detections != undefined) {
       if (detections.multiHandLandmarks != undefined) {
-        if(firstRecogMs == 0 && detections.multiHandLandmarks.length != 0){
+        if (firstRecogMs == 0 && detections.multiHandLandmarks.length != 0) {
           console.log("first detect at " + p.millis());
           firstRecogMs = p.millis();
-        } 
+        }
+
+    
+        for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
+          if (detections.multiHandedness[i].label == "Right") tromboneHUDworks = true;
+          if (detections.multiHandedness[i].label == "Left") drumsHUDworks = true;
+        }
         /*  p.drawLines([0, 5, 9, 13, 17, 0]); //palm
          p.drawLines([0, 1, 2, 3, 4]); //thumb
          p.drawLines([5, 6, 7, 8]); //index finger
@@ -107,6 +121,7 @@ let sketch = function (p) {
 
         p.calcHandSizes();
         p.drawHandIcons();
+        p.drawHUDIndicators();
 
         for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
           if (detections.multiHandedness[i].label == "Right") tromboneCurHand = i;
@@ -122,19 +137,26 @@ let sketch = function (p) {
     }
 
 
+
     //this is shitty and will break if only 1 hand is there
     if (trombone != undefined && timeNow > nextTrombone) {
       try {
-        trombone.play();
-      } catch (error) {}
-      nextTrombone = timeNow + 60000 / p.map(processedHandSizes[tromboneCurHand], 40, 200, 20, 120, true);
+        if(tromboneHUDworks){
+          console.log(tromboneRate);
+          trombone.rate(tromboneRate);
+          trombone.play();
+        } 
+      } catch (error) { }
+      nextTrombone = timeNow + 60000 / p.map(processedHandSizes[tromboneCurHand], 40, 200, 120, 20, true);
+      tromboneHUDworks = false;
     }
 
     if (snare != undefined && timeNow > nextSnare) {
       try {
-        snare.play();
-      } catch (error) {}
-      nextSnare = timeNow + 60000 / processedHandSizes[snareCurHand];
+        if(drumsHUDworks) snare.play();
+      } catch (error) { }
+      nextSnare = timeNow + 60000 / p.map(processedHandSizes[snareCurHand], 40, 200, 200, 40, true);
+      drumsHUDworks = false;
     }
 
 
@@ -142,18 +164,40 @@ let sketch = function (p) {
 
 
   p.drawDepthIndicator = function (firstRecogMs) {
-    let fadeTime = 4000;
-    if (depthImg != undefined && firstRecogMs == 0) {
-      p.image(depthImg, p.width / 2, p.height / 2, scaling * 200, scaling * 200);
+    let fadeTime = 3000;
+    let leColor = p.color("255");
+    if (handsFont != undefined && firstRecogMs == 0) {
+      leColor = p.color(0);
     } else if (firstRecogMs != 0 && p.millis() - firstRecogMs < fadeTime) {
-      let factor = p.map(p.millis() - firstRecogMs, 0, fadeTime, 0,1);
-      console.log(factor);
-      p.lerpColor(p.color(0), p.color(255), factor);
+      let factor = p.map(p.millis() - firstRecogMs, 0, fadeTime, 0, 1);
+      leColor = p.lerpColor(p.color(0), p.color(255), factor);
     }
+    p.fill(leColor);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textSize(scaling * 200);
+    p.textFont(handsFont);
+    p.text("C", p.width / 2, p.height / 2);
   }
 
-  p.drawRecognNotice = function (firstRecogMs){
-    
+  p.drawRecognNotice = function (firstRecogMs) {
+    let fadeTime = 3000;
+    let leColor = p.color("255");
+    if (handsFont != undefined && firstRecogMs == 0) {
+      leColor = p.color(50);
+    } else if (firstRecogMs != 0 && p.millis() - firstRecogMs < fadeTime) {
+      let factor = p.map(p.millis() - firstRecogMs, 0, fadeTime, 0, 1);
+      leColor = p.lerpColor(p.color(50), p.color(255), factor);
+    }
+    p.fill(leColor);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textSize(scaling * 120);
+    p.textFont(handsFont);
+    p.push();
+    p.translate(p.width / 3, p.height / 2);
+    p.scale(-1, 1);
+    p.text("A", 0, 0);
+    p.pop();
+    p.text("A", 2 * p.width / 3, p.height / 2);
   }
 
   p.drawLandmarks = function (indexArray, hue) {
@@ -199,25 +243,96 @@ let sketch = function (p) {
       p.push();
       p.translate((1 - detections.multiHandLandmarks[i][9].x) * p.width, detections.multiHandLandmarks[i][9].y * p.height)
       p.rotate(angle);
-      if (detections.multiHandedness[i].label == "Right") p.scale(-1, 1);
-      p.fill(0);
+      //right is really left, neo
+      let whichHand = "A";
+      if (detections.multiHandedness[i].label == "Right") {
+        p.scale(-1, 1);
+        whichHand = "B"
+        console.log(detections.multiHandLandmarks[i][5].y);
+        tromboneRate = p.map(detections.multiHandLandmarks[i][5].y, 0, 1, 1.5, 0.8);
+      }
+      p.fill(50);
       //using text workaround font because p5js doesnt't provide any SVG Capability
       p.textAlign(p.CENTER, p.CENTER);
       p.textSize(processedHandSizes[i] * 4 * scaling);
       p.textFont(handsFont);
-      p.text("A", 0, 0,);
+      p.text(whichHand, 0, 0);
       //p.image(leftHandImage, 0, 0, processedHandSizes[i] * 4 * scaling, processedHandSizes[i] * 4 * scaling);
-      if(i == snareCurHand) {
-        p.translate(scaling * 20, scaling *40);
-        p.image(drumsImg, 0, 0, processedHandSizes[i] * 1 * scaling, processedHandSizes[i] * 1 * scaling)
+
+      if (i == snareCurHand) {
+        p.push();
+        p.fill(0);
+        p.translate(scaling * 20, scaling * 40);
+        p.rotate(2 / 3 * p.PI);
+        p.translate(processedHandSizes[i] * -.2, processedHandSizes[i] * 2);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.textSize(processedHandSizes[i] * 4 * scaling);
+        p.textFont(handsFont);
+        p.text("E", 0, 0);
+        //p.image(drumsImg, 0, 0, processedHandSizes[i] * 1 * scaling, processedHandSizes[i] * 1 * scaling)
+        p.pop();
       }
-      if(i == tromboneCurHand) {
-        p.translate(scaling * 20, scaling *40);
-        p.image(tromboneImg, 0, 0, processedHandSizes[i] * 1 * scaling, processedHandSizes[i] * 1 * scaling)
+      if (i == tromboneCurHand) {
+        p.push();
+        p.fill(0);
+        p.translate(scaling * 20, scaling * 40);
+        p.scale(-1, 1);
+        p.rotate(.25 * p.PI);
+        p.translate(processedHandSizes[i] * -.2, processedHandSizes[i] * -.8);
+        //p.translate(0, 200);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.textSize(processedHandSizes[i] * 4 * scaling);
+        p.textFont(handsFont);
+        p.text("D", 0, 0);
+        //p.image(tromboneImg, 0, 0, processedHandSizes[i] * 1 * scaling, processedHandSizes[i] * 1 * scaling)
+        p.pop();
       }
       p.pop();
 
     }
+  }
+
+
+  p.drawHUDIndicators = function () {
+    loadingStill = false;
+    let totalnum = 4;
+    let tromboneHUDnum = 4;
+    let drumsHUDnum = 4;
+    let tromboneHUDworks = false;
+    let drumsHUDworks = false;
+
+    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
+      if (detections.multiHandedness[i].label == "Right") tromboneHUDworks = true;
+      if (detections.multiHandedness[i].label == "Left") drumsHUDworks = true;
+    }
+
+    tromboneHUDnum = p.floor(p.map(processedHandSizes[tromboneCurHand], 40, 200, 1.5, 5.5, true));
+    drumsHUDnum = p.floor(p.map(processedHandSizes[snareCurHand], 40, 200, 1.5, 5.5, true));
+
+    p.push();
+    p.fill(0);
+    p.translate(0, p.height - scaling * 40);
+    for (let i = 0; i < totalnum; i++) {
+      p.translate(40 * scaling, 0);
+      if (!tromboneHUDworks) p.fill("red");
+      else if (i < tromboneHUDnum) p.fill(0);
+      else p.fill(50);
+      p.ellipse(0, 0, 20, 20);
+    }
+    p.pop();
+
+    p.push();
+    p.fill(0);
+    p.translate(p.width, p.height - scaling * 40);
+    for (let i = 0; i < totalnum; i++) {
+      p.translate(-40 * scaling, 0);
+      if (!drumsHUDworks) p.fill("red");
+      else if (i < drumsHUDnum) p.fill(0);
+      else p.fill(50);
+      p.ellipse(0, 0, 20, 20);
+    }
+    p.pop()
+
   }
 
   p.calcHandSizes = function () {
